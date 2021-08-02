18d62563ddecbf8a2e58c248bfd5d460a26188fd
SvetlanaLyubimskaya <lyubimskaya.s@yandex.ru>
2021-08-02 12:50:07 +0300

    3 commit

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,6 +1,6 @@
 // import random from '../app';
 
 test('random', () => {
-    const value = 'result';
-    expect(value).toBe('result');
+  const value = 'result';
+  expect(value).toBe('result');
 });
