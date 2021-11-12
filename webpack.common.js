7d357d7e417aa7730c8fabe11f2186af1ac72e3c
SvetlanaLyubimskaya <lyubimskaya.s@yandex.ru>
2021-11-12 18:53:43 +0300

    edit file

--- webpack.common.js
+++ webpack.common.js
@@ -42,7 +42,7 @@ module.exports = {
                     {
                         loader: 'file-loader',
                         options: {
-                            // name: '[name].[ext]',
+                            name: '[name].[ext]',
                             outputPath: 'img/',
                             publicPath: 'img/',
                         },
